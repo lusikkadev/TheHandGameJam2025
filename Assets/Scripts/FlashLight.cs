25d33f3924a23d4368f7daec11bcdb6df9f69c2f
jmzn00 <mansikkajaakko13@gmail.com>
2025-11-01 17:18:17 +0200

    MoreFog

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -5,10 +5,19 @@ public class FlashLight : MonoBehaviour
     public float intensity = 1f;
     public bool isOn = false;
 
+    [SerializeField] private FogController fogController; // REPLACE DIRECT REFERENCE
+
+    private void Awake()
+    {
+        TurnOn();
+    }
+
     public void TurnOn()
     {
         isOn = true;
         gameObject.SetActive(true);
+
+        fogController.SetPlayerLights(true);
     }
 
     public void TurnOff()
@@ -16,5 +25,6 @@ public class FlashLight : MonoBehaviour
         isOn = false;
         gameObject.SetActive(false);
 
+        fogController.SetPlayerLights(false);
     }
 }
---
+++ Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class ParallaxLayer : MonoBehaviour
+{
+    [Tooltip("Parallax scroll speed relative to camera movement")]
+    public float speedX;
+    public float speedY;
+
+    Transform cameraTransform;
+    Vector3 previousCameraPosition;
+    bool previousMoveParallaxLayers;
+    ParallaxOptions po;
+
+    void OnEnable()
+    {
+        var cam = Camera.main;
+        cameraTransform = cam.transform;
+        po = cam.GetComponent<ParallaxOptions>();
+        previousCameraPosition = cameraTransform.position;
+    }
+
+    void Update()
+    {
+        if (po.moveParallaxLayers && !previousMoveParallaxLayers)
+            previousCameraPosition = cameraTransform.position;
+
+        previousMoveParallaxLayers = po.moveParallaxLayers;
+
+        if (!Application.isPlaying && !po.moveParallaxLayers)
+            return;
+
+        var delta = cameraTransform.position - previousCameraPosition;
+        transform.position += Vector3.Scale(delta, new Vector3(speedX, speedY));
+        previousCameraPosition = cameraTransform.position;
+    }
+}
