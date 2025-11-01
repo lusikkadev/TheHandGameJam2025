fc15cfe476d2be8d63e49816850692401630f918
lusikkadev <tatuketonen@gmail.com>
2025-11-01 22:58:49 +0200

    Added Flashlight Energy

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -1,30 +1,81 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class FlashLight : MonoBehaviour
 {
-    public float intensity = 1f;
+    public float energy;
+    public float maxEnergy = 20f;
+
     public bool isOn = false;
 
     [SerializeField] private FogController fogController; // REPLACE DIRECT REFERENCE
 
+
+    private Light2D[] lights;
+    private Renderer[] renderers;
+    private ParticleSystem[] particleSystems;
+
     private void Awake()
     {
+        lights = GetComponentsInChildren<Light2D>(true);
+        renderers = GetComponentsInChildren<Renderer>(true);
+        particleSystems = GetComponentsInChildren<ParticleSystem>(true);
+
+
         TurnOn();
+    }
+
+    private void Start()
+    {
+        energy = maxEnergy;
     }
 
     public void TurnOn()
     {
         isOn = true;
-        gameObject.SetActive(true);
+        SetVisualsActive(true);
 
-        fogController.SetPlayerLights(true);
+        fogController?.SetPlayerLights(true);
     }
 
     public void TurnOff()
     {
         isOn = false;
-        gameObject.SetActive(false);
+        SetVisualsActive(false);
 
-        fogController.SetPlayerLights(false);
+        fogController?.SetPlayerLights(false);
+    }
+
+    private void SetVisualsActive(bool value)
+    {
+        foreach (var l in lights) l.enabled = value;
+        foreach (var r in renderers) r.enabled = value;
+
+        foreach (var ps in particleSystems)
+        {
+            if (value) ps.Play();
+            else ps.Stop();
+        }
+    }
+
+    private void Update()
+    {
+        if (isOn)
+        {
+            energy -= 1f * Time.deltaTime;
+
+            // auto turn off when drained
+            if (energy <= 0f)
+            {
+                energy = 0f;
+                TurnOff();
+            }
+        }
+        else
+        {
+            energy += 0.5f * Time.deltaTime;
+        }
+
+        energy = Mathf.Clamp(energy, 0f, maxEnergy);
     }
 }
