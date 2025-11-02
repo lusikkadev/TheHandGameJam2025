50c1396f2bb205360d8a641e6425a77018ef8bfe
jmzn00 <mansikkajaakko13@gmail.com>
2025-11-02 13:43:39 +0200

    Fogging

--- Assets/FogStuff/FogController.cs
+++ Assets/FogStuff/FogController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using System;
 
 public class FogController : MonoBehaviour
 {
@@ -8,6 +9,25 @@ public class FogController : MonoBehaviour
     [SerializeField] private float remDist = -1f;
     [SerializeField] private List<Transform> lightPositions;
     [SerializeField] private Renderer fogRend;
+
+    Color cachedColor;
+
+    float cachedXNoiseScale;
+    float cachedYNoiseScale;
+    float cachedNoiseMult;
+
+    private void Awake()
+    {
+        cachedColor = fogRend.material.GetColor("_BaseColor");
+
+        cachedNoiseMult = fogRend.material.GetFloat("_NoiseMult");
+        cachedXNoiseScale = fogRend.material.GetFloat("_XNoiseScale");
+        cachedYNoiseScale = fogRend.material.GetFloat("_YNoiseScale");
+    }
+
+    
+    
+    
     private void Update()
     {
         fogRend.material.SetVector("_Light0Pos", lightPositions[0].position);
@@ -20,6 +40,15 @@ public class FogController : MonoBehaviour
         
         fogRend.material.SetFloat("_FadeDistance", fadeDist);
         fogRend.material.SetFloat("_Rem", remDist);
+
+        if (Input.GetKeyDown(KeyCode.V)) 
+        {
+            OnPlayerGrabbed();
+        }
+        if (Input.GetKeyDown(KeyCode.C)) 
+        {
+            ResetGrabbed();
+        }
     }
 
     public void SetPlayerLights(bool value) 
@@ -35,5 +64,51 @@ public class FogController : MonoBehaviour
             fogRend.material.SetFloat("_Light0Rem", remDist);
         }
         
+    }
+    private void ResetGrabbed() 
+    {
+        StopAllCoroutines();
+
+        fogRend.material.SetColor("_BaseColor", cachedColor);
+        fogRend.material.SetFloat("_XNoiseScale", cachedXNoiseScale);
+        fogRend.material.SetFloat("_YNoiseScale", cachedYNoiseScale);
+        fogRend.material.SetFloat("_NoiseMult", cachedNoiseMult);
+    }
+    public void OnPlayerGrabbed() 
+    {
+        StartCoroutine(PlayerGrabbedEffectCo());
+    }
+    [SerializeField] private float alphaAddAmt = 0.1f;
+    [SerializeField] private float coDelay = 0.1f;
+
+    [SerializeField] private float xAdd = 0.1f;
+    [SerializeField] private float yAdd = 0.1f;
+    [SerializeField] private float mAdd = 0.1f;
+   
+    private IEnumerator PlayerGrabbedEffectCo() 
+    {
+        Color c = cachedColor;
+
+        float x = cachedXNoiseScale;
+        float y = cachedYNoiseScale;
+        float m = cachedNoiseMult;
+
+        while (true) 
+        {
+            yield return new WaitForSeconds(coDelay);
+
+            c.a += alphaAddAmt;
+            c.a = Mathf.Clamp(c.a, 0, 1);
+
+            fogRend.material.SetColor("_BaseColor", c);
+
+            x += xAdd;
+            y += yAdd;
+            m += mAdd;
+
+            fogRend.material.SetFloat("_XNoiseScale", x);
+            fogRend.material.SetFloat("_YNoiseScale", y);
+            fogRend.material.SetFloat("_NoiseMult", m);
+        }
     }
 }
